c9afd429a97d2c97019e2061ae04607165306b6d
Anil Kumar <anil.apex@outlook.com>
2019-07-05 02:00:33 +0530

    Practice Fractional Knapsack.
    Related Work Items: #779, #790

--- FractionalKnapsack.cs
+++ FractionalKnapsack.cs
@@ -1,6 +1,5 @@
 ﻿using static System.Console;
 using System;
-using System.Linq;
 
 namespace Greedy
 {
@@ -8,20 +7,31 @@ namespace Greedy
     {
         private int CalculateMaxValue(Item[] items, int knapCapacity)
         {
-            items = items.OrderByDescending(i => i.ValuePerKg).ToArray();
+            Array.Sort(items, (i1, i2) => -1 * i1.ValuePerKg.CompareTo(i2.ValuePerKg));
 
 
-            return CalculateMaxValue_(0, knapCapacity);   // Start w/ 1st (most costly) item
+            return FindMaxItem(0, knapCapacity);   // Start w/ 1st (most costly) item
 
 
 
-            int CalculateMaxValue_(int i, int knapCap)
+            int FindMaxItem(int i, int knapCap)
             {
                 if (knapCap == 0 || i == items.Length) return 0;
 
-                return items[i].Weight <= knapCap   // Check can current item be added fully?
-                    ? items[i].Value + CalculateMaxValue_(i + 1, knapCap - items[i].Weight)   // Take full item
-                    : (int)Math.Round(knapCap * items[i].ValuePerKg);   // Take partial item
+
+                if (items[i].Weight <= knapCap)   // Check can current item be added fully?
+                {
+                    WriteLine(items[i]);   // Print full item
+
+                    knapCap -= items[i].Weight;
+                    return items[i].Value + FindMaxItem(i + 1, knapCap);
+                }
+
+                // Find partial item quantity & value
+                int weight = knapCap,
+                    value = (int) Math.Round(items[i].ValuePerKg * weight);
+                WriteLine($"{items[i].Name}: {weight} Kg, {value:C0}");   // Print partial item
+                return value;
             }
         }
 
@@ -39,8 +49,8 @@ namespace Greedy
                 new Item("Item 6", 3,5 )
 
                 //new Item("Item 1", 20, 100),
-                //new Item("Item 1", 30, 120),
-                //new Item("Item 1", 10, 60)
+                //new Item("Item 2", 30, 120),
+                //new Item("Item 3", 10, 60)
             };
             //int knapCapacity = 50;
             int knapCapacity = 10;
@@ -59,12 +69,16 @@ namespace Greedy
             Name = name;
             Weight = weight;
             Value = value;
-            ValuePerKg = (float)Value / (float)Weight;
+            ValuePerKg = (float) Value / (float) Weight;
         }
 
         public string Name { get; }
         public int Weight { get; }
         public int Value { get; }
         public float ValuePerKg { get; }
+
+
+        /// <inheritdoc />
+        public override string ToString() => $"{Name}: {Weight} Kg, {Value:C0}";
     }
 }
