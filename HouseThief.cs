2d74fe60f85726270f0daec1a3929f8fc562a622
Anil Kumar <anil.apex@outlook.com>
2019-07-26 20:00:50 +0530

    Find neighbours using Dictionary.
    Related Work Items: #782, #790

--- HouseThief.cs
+++ HouseThief.cs
@@ -7,10 +7,10 @@ namespace Greedy
 {
     public class HouseThief
     {
-        private (List<(int, int)> values, int total) MaximizeValue(int[] houses)
+        private (Dictionary<int, int> values, int total) MaximizeValue(int[] houses)
         {
             int index = 0;
-            List<(int value, int i)> stolenValues = new List<(int, int)>(houses.Length / 2 + 1);
+            Dictionary<int, int> stolenValues = new Dictionary<int, int>(houses.Length / 2 + 1);
             (int value, int i) previousHouse = default;
             (int value, int i)[] housesIndexes = Array.ConvertAll(houses, v => (v, index++));
 
@@ -28,17 +28,21 @@ namespace Greedy
                 int currentHouseValue = 0;
                 if (current == 0)
                 {
-                    stolenValues.Add(housesIndexes[0]);
+                    stolenValues[housesIndexes[0].i] = housesIndexes[0].value;
+                    //stolenValues.Add(housesIndexes[0]);
                     previousHouse = housesIndexes[0];
                     currentHouseValue = housesIndexes[0].value;
                     current++;
                 }
 
                 bool isAdjacent = Math.Abs(previousHouse.i - housesIndexes[current].i) == 1 ||
-                                  stolenValues.Any(h => Math.Abs(h.i - housesIndexes[current].i) == 1);
+                                  stolenValues.ContainsKey(housesIndexes[current].i + 1) ||
+                                  stolenValues.ContainsKey(housesIndexes[current].i - 1);
+                //stolenValues.Any(h => Math.Abs(h.i - housesIndexes[current].i) == 1);
                 if (!isAdjacent)
                 {
-                    stolenValues.Add(housesIndexes[current]);
+                    stolenValues[housesIndexes[current].i] = housesIndexes[current].value;
+                    //stolenValues.Add(housesIndexes[current]);
                     previousHouse = housesIndexes[current];
                     currentHouseValue += housesIndexes[current].value;
                 }
@@ -51,11 +55,11 @@ namespace Greedy
 
         internal static void Work()
         {
-            //int[] houses = { 6, 7, 1, 30, 8, 2, 4 };
-            int[] houses = { 20, 5, 1, 13, 6, 11, 40 };
+            int[] houses = { 6, 7, 1, 30, 8, 2, 4 };
+            //int[] houses = { 20, 5, 1, 13, 6, 11, 40 };
 
             var (stolenValues, total) = new HouseThief().MaximizeValue(houses);
-            foreach ((int value, var _) in stolenValues) Write($"{value:C0}  ");
+            foreach ((int _, var value) in stolenValues) Write($"{value:C0}  ");
             WriteLine($"\nTotal: {total}");
         }
     }
