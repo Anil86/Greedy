79b9c56743ecb1457c2d2ce69f89eb9a11a833cd
Anil Kumar <anil.apex@outlook.com>
2019-07-29 21:14:41 +0530

    Re-practice: Addem Up
    Related Work Items: #772

---
+++ AddemUp.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using static System.Console;
+
+namespace Greedy
+{
+    public class AddemUp
+    {
+        private (int value, int cost) RecycleEfficiently(int[] cards)
+        {
+            Array.Sort(cards);
+
+            int cost = 0;
+            RecycleEfficiently(0, 1);
+            int value = cards[cards.Length - 1];   // Last card's value
+
+            return (value, cost);
+
+
+
+            void RecycleEfficiently(int previous, int current)
+            {
+                if (current == cards.Length) return;
+
+
+                // Replace previous 2 cards w/ new recycled card
+                cards[current] = cards[previous] + cards[current];
+                cost += cards[current];   // Add new cost to previous cost
+
+                // Sort to place new higher value card in proper position
+                Array.Sort(cards, current, cards.Length - current);
+
+
+                RecycleEfficiently(current, current + 1);   // Recycle remaining cards
+            }
+
+        }
+
+
+
+        internal static void Work()
+        {
+            int[] cards = { 1, 4, 3, 4 };
+            //int[] cards = { 9, 9, 9, 9, 9, 9, 9, 9, 9 };
+            //int[] cards =
+            //{
+            //    15282, 6674, 93033, 48628, 75335, 61596, 66495, 33570, 15004, 60598, 91072, 79972, 78971, 72325, 15986,
+            //    95574, 41770, 39882, 96387, 9413
+            //};
+
+            (int value, int cost) = new AddemUp().RecycleEfficiently(cards);
+            WriteLine($"Card: {value}\tCost: {cost:C0}");
+        }
+    }
+}
